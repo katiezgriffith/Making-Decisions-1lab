a1fca6d7b114e1bbd12114b49a34fe6575072625
Katie Devops <angel_9586@hotmail.com>
2021-10-13 09:57:48 -0500

    First interation

---
+++ Desktop/Dev/challenge-find-the-range/findTheRange.js
@@ -0,0 +1,8 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+// Write your solution below:
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index])
+  }
+  
