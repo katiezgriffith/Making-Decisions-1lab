d92424c5a199fdb8e610c3de4db3d5786346c50a
Katie Devops <angel_9586@hotmail.com>
2021-10-14 14:19:36 -0500

    second commit second problem

--- Desktop/Dev/making-decisions-2/makingDecisions2Lab.js
+++ Desktop/Dev/making-decisions-2/makingDecisions2Lab.js
@@ -7,6 +7,7 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+let firstItem = arr[0]
 
 
 ////////// PROBLEM 2 //////////
@@ -15,6 +16,9 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+let lastItemRemoved = arr.pop()
+  console.log(lastItemRemoved)
+
 
 ////////// PROBLEM 3 //////////
 
