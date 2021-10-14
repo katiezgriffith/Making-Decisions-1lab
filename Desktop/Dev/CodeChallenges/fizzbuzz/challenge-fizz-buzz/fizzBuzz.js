53d9d41f6681e210ba5aac382e7b3d1a51ceaa39
Katie Devops <angel_9586@hotmail.com>
2021-10-14 09:42:32 -0500

    Getting it

--- Desktop/Dev/CodeChallenges/fizzbuzz/challenge-fizz-buzz/fizzBuzz.js
+++ Desktop/Dev/CodeChallenges/fizzbuzz/challenge-fizz-buzz/fizzBuzz.js
@@ -1,1 +1,13 @@
 // Write your solution below this line:
+function fizzBuzz(start, end){
+    for(let num=start; num <= end; num++){
+      //If the number is a multiple of 3 log “Fizz” instead of the number
+      //If the number is a multiple of 5 log “Buzz” instead of the number
+      //If the number is a multiple of 3 AND 5 log “FizzBuzz” instead of the number
+      //If not logging “Fizz”, “Buzz”, or “FizzBuzz” log the number
+    }
+}
+
+
+
+
