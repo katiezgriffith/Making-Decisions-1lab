18e52415cdab709b1413a5dd614bb807a5f39565
Katie Devops <angel_9586@hotmail.com>
2021-10-14 15:07:13 -0500

    fifth problem still in process

--- Desktop/Dev/making-decisions-2/makingDecisions2Lab.js
+++ Desktop/Dev/making-decisions-2/makingDecisions2Lab.js
@@ -70,7 +70,33 @@ var score = 74
 
 //Code Here
 
+If (score >= 90);{
+console.log ("A")
+}
 
+
+  else if 
+  (score >= 80 || score <= 89){
+  console.log ("B")
+}
+
+
+  else if 
+  (score >= 70 || score <= 79) {
+  console.log ("C")
+
+}
+
+
+  else if 
+  (score >= 60 || score <= 69) {
+  console.log ('D')
+
+
+}
+ else 
+ {console.log ('F')
+}
 ////////// Intermediate Problems //////////
 
 ////////// PROBLEM 6 //////////
