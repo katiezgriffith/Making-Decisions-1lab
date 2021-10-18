95b4221caacd01d7e7cca88e086189030d29aab0
Katie Devops <angel_9586@hotmail.com>
2021-10-18 09:38:00 -0500

    initial commit

--- Desktop/Dev/CodeChallenges/challenge-add-to-zero/addToZero.js
+++ Desktop/Dev/CodeChallenges/challenge-add-to-zero/addToZero.js
@@ -2,3 +2,15 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+let arr1 = [1, 4, 11, 2, 37, -4]
+let arr2 = [0, 21, 33, 6, 0, -9]
+let arr3 = [0, 1, 2, 3, 4, 5}
+
+    for (let i = 0; i < array.length ; i++)
+        
+        if (array ))
+
+    for (let x = i + 1; x < array.length; i++)
+
+    
