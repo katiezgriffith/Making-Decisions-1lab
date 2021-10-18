dafaf1bf0d288aa165052fe71944e1edae89b789
Katie Devops <angel_9586@hotmail.com>
2021-10-18 09:59:56 -0500

    Added for loops and if statements

--- Desktop/Dev/CodeChallenges/challenge-add-to-zero/addToZero.js
+++ Desktop/Dev/CodeChallenges/challenge-add-to-zero/addToZero.js
@@ -2,15 +2,19 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+function findWhattwonumbersequalzero ()
 
 let arr1 = [1, 4, 11, 2, 37, -4]
 let arr2 = [0, 21, 33, 6, 0, -9]
-let arr3 = [0, 1, 2, 3, 4, 5}
+let arr3 = [0, 1, 2, 3, 4, 5]
 
-    for (let i = 0; i < array.length ; i++)
-        
-        if (array ))
+    for (let i = 0; i < array.length ; i++) 
 
     for (let x = i + 1; x < array.length; i++)
+
+    If (array(i) + array(x) === 0); { 
+        console.log ("True")
+    }
+
 
     
